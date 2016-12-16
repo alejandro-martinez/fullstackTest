3797162726051919794520f450712347c132f0c8
Alejandro <alejandromartinezap@gmail.com>
2016-12-15 22:50:57 -0300

    Ajustes estilo

--- backend/controllers/client.js
+++ backend/controllers/client.js
@@ -3,7 +3,7 @@ var models  = require('../models/index');
 module.exports = function( app ) {
 	
 	app.get('/clients', function(req, res, next) {
-		res.json([{id:1,name:"Ale",email:"a@g.com",phone:"123123",providers:[{id:1, name:"prov 1"}]}])
+		
 		var onFind = function( clients ) {
 			res.json( clients );
 		};
